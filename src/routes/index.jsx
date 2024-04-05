b78ca81d280cb43d8a7f0f9dfb02f16e388cbdc7
muratokur3 <muratokur3@icloud.com>
2024-04-05 10:10:34 +0300

    master

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import {  HashRouter, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Layout from "../layout";
@@ -6,14 +6,12 @@ import Welcome from "../pages/Welcome";
 
 function LayoutRoute() {
   return (
-    <HashRouter>
       <Routes>
         <Route path="/*" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="welcome" element={<Welcome />} />
         </Route>
       </Routes>
-      </HashRouter>
   );
 }
 

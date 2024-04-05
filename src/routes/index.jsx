6da4f9e693a8fcf83e9f57fa28cefbddd44b4343
muratokur3 <muratokur3@icloud.com>
2024-04-05 08:56:53 +0300

    master

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,15 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "../layout";
 import Welcome from "../pages/Welcome";
+import { Router } from "express";
 
 function LayoutRoute() {
   return (
-    <Routes>
-      <Route path="/*" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="welcome" element={<Welcome/>} />
-      </Route>
-    </Routes>
+    <Router basename="/TheWeather">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="welcome" element={<Welcome />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
